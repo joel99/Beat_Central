3df86d667e6d5e71a8d3d1b081d0288dde1439da
Mikhail Kotlik <mkotlik@stuy.edu>
2016-12-06 16:56:23 -0500

    Added post functionality to rest

--- Beats/utils/rest.py
+++ Beats/utils/rest.py
@@ -5,6 +5,8 @@
 import urllib2
 import urllib
 
+#Maybe should use add_header() instead of headers?
+
 def get(url, query = None, headers = None):
     response = getR(url, query, headers)
     result = {"url":response.geturl(), "headers":response.info(),
@@ -16,11 +18,21 @@ def getR(url, query = None, headers = None):
     response = urllib2.urlopen(req)
     return response
 
+def post(url, body, query = None, headers = None):
+    response = postR(url, body, query, headers)
+    result = {"url":response.geturl(), "headers":response.info(),
+              "status":response.getcode(), "body":response.read()}
+    return result
+
+def postR(url, body, query = None, headers = None):
+    data = urllib.urlencode(body)
+    req = urllib2.Request(url + format_query(query), data, headers)
+    response = urllib2.urlopen(req)
+    return response
+
 #How do None types work in python?
 def format_query(query):
     q_string = ""
-    if query = None:
-        return q_string
     for key in query:
         q_string += key + query[key] + "&"
         result = q_string[:-1].replace(" ", "+")
