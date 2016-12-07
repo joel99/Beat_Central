544052b5516b642db998b558b3dd808356008d0c
Mikhail Kotlik <mkotlik@stuy.edu>
2016-12-07 14:50:30 -0500

    WIP on lastFm module

--- Beats/utils/lastFm.py
+++ Beats/utils/lastFm.py
@@ -4,6 +4,7 @@
 #Use MKotlik last.fm account for API account
 
 import rest
+import json
 
 #API_KEY is temporarily harcoded, will later be moved to outside file
 API_KEY = "42becc236bc618eb1ba223b81fa9e4f8"
@@ -13,11 +14,11 @@ API_ROOT = "http://ws.audioscrobbler.com/2.0/"
 def search_songs(name, artist = None):
     query_d = {"method":"track.search", "track":name, "api_key":API_KEY,
                "format":"json"}
-    if artist != NONE:
+    if artist != None:
         query_d["artist"] = artist
     url = API_ROOT
     res = rest.get(url, query_d) #User-agent header should be default
     res_type = res["type"]
-    if res_type == HTTPError || res_type == URLError:
+    if res_type == "HTTPError" or res_type == "URLError":
         return rest.explain_error(res["object"])
-    return (res["object"].getcode(), res["object"].info())
+    return res["object"]
