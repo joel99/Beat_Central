3500d3a1b8f68b759284d847bb49b0176f3bc722
MKotlik <mkotlik@stuy.edu>
2016-12-12 21:14:59 -0500

    Completed and documented spotify.py

--- Beats/utils/spotify.py
+++ Beats/utils/spotify.py
@@ -3,56 +3,61 @@
 # spotify API functions
 # Team Bass Drop - Beat Central
 
-# Please use an identifiable User-Agent header on all requests.
-# ^^^This implies that I need to keep the urrlib2 header identifier,
-#   and use add_header for our header/auth purposes?
-
 import rest
 import json
 from base64 import b64encode
 
-# NOTES:
-# NOTE: Access tokens have limited lives, should implement refresh system
-# TODO: create before_first_request func in app.py to pass auth info to auth
 
 # GLOBALS:
 API_ROOT = "https://api.spotify.com/v1"
-# ACCESS_TOKEN = ""
 
-# auth not needed, unable to use market=from_token for improved rates
-"""
-def auth(client_id, client_secret):
-    # returns boolean
-    auth_str = b64encode(client_id + ":" + client_secret)
-    auth_hdr = {"Authorization": "Basic " + auth_str}
-    url = "https://accounts.spotify.com/api/token"
-    body = {"grant_type": "client_credentials", "scope": "user-read-private"}
-    q_str = {"scope": "user-read-private"}
-    res = rest.post(url, body, q_str, auth_hdr)
-    if res["type"] == "HTTPError" or res["type"] == "URLError":
-        # returning (status, result); error already sent to console
-        return False
-    res_dict = json.loads(res["object"].read())
-    if "error" in res_dict:
-        err_str = "ERROR: spotify.auth() failed with error: '"
-        err_str += res_dict["error"] + "'/\n & error_description: '"
-        err_str += res_dict["error_description"]
-        print err_str
-        return False
-    global ACCESS_TOKEN
-    ACCESS_TOKEN = res_dict["access_token"]
-    return True
-"""
+
+# # # === MAIN FUNCTION === # # #
+
+
+def getItemUri(q_type, name, artist=None):
+    """ Returns the Spotify URI for an item.
+
+    The Spotify URI is needed to embed a Spotify player widget for an item in
+    the item webpage.
+
+    Args:
+        q_type (str): item type - can be 'album', 'artist', or 'track'
+        name (str): name of the item
+        artist (str, optional): item's artist. REQUIRED IF q_type IS NOT ARTIST
+
+    Returns:
+        tuple: first element is a string status ("Error" or "OK"). Second
+            element is the item's spotify URI string.
+
+        An "Error" status is returned, along with an empty URI, if an HTTP,
+        URL, or API error was encountered during operation.
+
+        The returned object's structure is as follows::
+            ("<OK || Error>", "<SPOTIFY URI>")
+    """
+    item = search(q_type, name, artist)
+    if item[0] == "Error":
+        return ("Error", "")
+    return ("OK", item[1]["uri"])
+
+
+# # # === HELPER FUNCTIONS === # # #
 
 
 def search(q_type, name, artist=None):
     """ Get 1st search result for album, artist, or track.
 
-    Accepts types: album, artist, and track
-    Needs artist for album and track searches
+    Args:
+        q_type (str): item type - can be 'album', 'artist', or 'track'
+        name (str): name of the item
+        artist (str, optional): item's artist. REQUIRED IF q_type IS NOT ARTIST
 
-    Returns tuple, where 1st el is status, and second el is 1st search
-        result. If empty, no results found.
+    Returns:
+        tuple: first element is a string status ("Error" or "OK"). Second
+            element is a dictionary containing the name, type, and URI of the
+            item, in addition to the artist (if the item isn't an artist).
+            The keys to the fields are "name", "type", "uri", and "artist".
     """
     url = API_ROOT + "/search"
     # auth_hdr = {"Authorization": "Bearer " + ACCESS_TOKEN}
@@ -94,13 +99,34 @@ def build_result_dict(q_type, item_list):
     return res_dict
 
 
-def getItemUri(q_type, name, artist=None):
-    """ Returns the Spotify uri for an item
+# # # === UNUSED CODE === # # #
 
-    Args:
 
-    """
-    item = search(q_type, name, artist)
-    if item[0] == "Error":
-        return ("Error", "")
-    return ("OK", item[1]["uri"])
+# auth not needed, unable to use market=from_token for improved rates
+""" If it does work, note that:
+- Access tokens have limited lives, should implement refresh system
+- Should create before_first_request func in app.py to pass auth info to auth
+"""
+"""
+def auth(client_id, client_secret):
+    # returns boolean
+    auth_str = b64encode(client_id + ":" + client_secret)
+    auth_hdr = {"Authorization": "Basic " + auth_str}
+    url = "https://accounts.spotify.com/api/token"
+    body = {"grant_type": "client_credentials", "scope": "user-read-private"}
+    q_str = {"scope": "user-read-private"}
+    res = rest.post(url, body, q_str, auth_hdr)
+    if res["type"] == "HTTPError" or res["type"] == "URLError":
+        # returning (status, result); error already sent to console
+        return False
+    res_dict = json.loads(res["object"].read())
+    if "error" in res_dict:
+        err_str = "ERROR: spotify.auth() failed with error: '"
+        err_str += res_dict["error"] + "'/\n & error_description: '"
+        err_str += res_dict["error_description"]
+        print err_str
+        return False
+    global ACCESS_TOKEN
+    ACCESS_TOKEN = res_dict["access_token"]
+    return True
+"""
